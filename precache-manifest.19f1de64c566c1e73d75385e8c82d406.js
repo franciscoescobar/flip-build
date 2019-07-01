7bbbe353db97282236879225302229abf8158598
franciscoescobar <franciscoescobar@users.noreply.github.com>
2019-07-01 17:24:05 -0300

    Initial commit

---
+++ precache-manifest.19f1de64c566c1e73d75385e8c82d406.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c128f59e648d6a4e95f810058fadf70",
+    "url": "/flip/index.html"
+  },
+  {
+    "revision": "1bf59a9027b4c19f9102",
+    "url": "/flip/static/css/main.e6e14695.chunk.css"
+  },
+  {
+    "revision": "d4d041f8ea23e40cdd50",
+    "url": "/flip/static/js/2.38449a4f.chunk.js"
+  },
+  {
+    "revision": "1bf59a9027b4c19f9102",
+    "url": "/flip/static/js/main.628dc93f.chunk.js"
+  },
+  {
+    "revision": "adfcd50c11b9300b7d88",
+    "url": "/flip/static/js/runtime~main.43d8d01f.js"
+  },
+  {
+    "revision": "17ae9cdd5bbdd05fb7efd98369d0e388",
+    "url": "/flip/static/media/00.17ae9cdd.jpg"
+  },
+  {
+    "revision": "146b7e3ee3c784b7a743d5fff6e5e2eb",
+    "url": "/flip/static/media/01_orinetacion.146b7e3e.jpg"
+  },
+  {
+    "revision": "b553b2bdd6e9e160386b74d1f0dae2a3",
+    "url": "/flip/static/media/01_orinetacion1.b553b2bd.jpg"
+  },
+  {
+    "revision": "623efaad910451a8d796f9c1ff576c21",
+    "url": "/flip/static/media/02_ventilacion.623efaad.jpg"
+  },
+  {
+    "revision": "3896c5cdcf620cf79380dcd8fff00f31",
+    "url": "/flip/static/media/02_ventilacion1.3896c5cd.jpg"
+  },
+  {
+    "revision": "fd4906d4941ad21cbc75737ca195ce02",
+    "url": "/flip/static/media/03-asoleamiento.fd4906d4.jpg"
+  },
+  {
+    "revision": "b7306395b2a0a32dd3e35e294ea463a4",
+    "url": "/flip/static/media/03-asoleamiento1.b7306395.jpg"
+  },
+  {
+    "revision": "f99e5bd2121efef3616dffb080f01fb8",
+    "url": "/flip/static/media/04-protecciones.f99e5bd2.jpg"
+  },
+  {
+    "revision": "e93cb213e5ed2eaec244b9bfb70534e1",
+    "url": "/flip/static/media/04-protecciones1.e93cb213.jpg"
+  },
+  {
+    "revision": "e6543fe7a33cd18036832948fff87ff8",
+    "url": "/flip/static/media/05-recuperacion.e6543fe7.jpg"
+  },
+  {
+    "revision": "0044d65623538e4a745e3f5d0f899462",
+    "url": "/flip/static/media/05-recuperacion1.0044d656.jpg"
+  },
+  {
+    "revision": "7a81893fbefa13c6e37393a79f4a6c70",
+    "url": "/flip/static/media/06-tratamiento.7a81893f.jpg"
+  },
+  {
+    "revision": "a2c43ba4dd735905910a9fc4095522ab",
+    "url": "/flip/static/media/06-tratamiento1.a2c43ba4.jpg"
+  },
+  {
+    "revision": "d50f1ddcea816ab724ebbe1862364f7f",
+    "url": "/flip/static/media/07-inercia-cubiertas.d50f1ddc.jpg"
+  },
+  {
+    "revision": "18e0a958a113983071c07e59b17761ed",
+    "url": "/flip/static/media/07-inercia-cubiertas1.18e0a958.jpg"
+  },
+  {
+    "revision": "d50f1ddcea816ab724ebbe1862364f7f",
+    "url": "/flip/static/media/07-inercia-losas.d50f1ddc.jpg"
+  },
+  {
+    "revision": "2b5a87cc4024244111d9f1e977fe4b70",
+    "url": "/flip/static/media/07-inercia-losas1.2b5a87cc.jpg"
+  },
+  {
+    "revision": "d50f1ddcea816ab724ebbe1862364f7f",
+    "url": "/flip/static/media/07-inercia-menu.d50f1ddc.jpg"
+  },
+  {
+    "revision": "d71036b765d8910a91f62fe3fe7cef0b",
+    "url": "/flip/static/media/07-inercia-menu1.d71036b7.jpg"
+  },
+  {
+    "revision": "1fe94ab70329be1df53710d7cb7f661d",
+    "url": "/flip/static/media/07-inercia-muro.1fe94ab7.jpg"
+  },
+  {
+    "revision": "1406ac95d0b091e17c40a68f2bcba437",
+    "url": "/flip/static/media/07-inercia-muro1.1406ac95.jpg"
+  },
+  {
+    "revision": "d50f1ddcea816ab724ebbe1862364f7f",
+    "url": "/flip/static/media/07-inercia-pisos.d50f1ddc.jpg"
+  },
+  {
+    "revision": "5092b5351c068bd070ffbc8740ef67f7",
+    "url": "/flip/static/media/07-inercia-pisos1.5092b535.jpg"
+  },
+  {
+    "revision": "5b01328ef99cb1c2a1dc92a22577ec6c",
+    "url": "/flip/static/media/07-inercia-terreno.5b01328e.jpg"
+  },
+  {
+    "revision": "13621a4944ed09545891643495b8682f",
+    "url": "/flip/static/media/07-inercia-terreno1.13621a49.jpg"
+  },
+  {
+    "revision": "528f44b2cd45ff93352d222d14500917",
+    "url": "/flip/static/media/08-solar.528f44b2.jpg"
+  },
+  {
+    "revision": "5ec66ae71ea42d9a352ccd270566e509",
+    "url": "/flip/static/media/08-solar1.5ec66ae7.jpg"
+  },
+  {
+    "revision": "a3f18ca6fcf3501d3bb3c1b52d684ad0",
+    "url": "/flip/static/media/09-geotermica.a3f18ca6.jpg"
+  },
+  {
+    "revision": "a56ca488ee9bba1aa60d0662bf678f2d",
+    "url": "/flip/static/media/09-geotermica1.a56ca488.jpg"
+  },
+  {
+    "revision": "2ff96073a976b292928c1b65ed074387",
+    "url": "/flip/static/media/10-eficiencia-geotermica a.2ff96073.jpg"
+  },
+  {
+    "revision": "c40b2a2573c4234a7e54e59903fcefdb",
+    "url": "/flip/static/media/10-eficiencia-geotermica a1.c40b2a25.jpg"
+  },
+  {
+    "revision": "45bb62c9ad8fba2232892c4090823472",
+    "url": "/flip/static/media/10-eficiencia-geotermica a2.45bb62c9.jpg"
+  },
+  {
+    "revision": "66fdb64ed869e5966d3617a82edabd5b",
+    "url": "/flip/static/media/10-eficiencia-menu.66fdb64e.jpg"
+  },
+  {
+    "revision": "426b34b71797a08947de80e2c0f5080c",
+    "url": "/flip/static/media/10-eficiencia-menu1.426b34b7.jpg"
+  },
+  {
+    "revision": "53066bb0312e9e10f5fd32abc8be2e82",
+    "url": "/flip/static/media/10-eficiencia-solar.53066bb0.jpg"
+  },
+  {
+    "revision": "f2925c78da8906575e8ba54cf3850bb4",
+    "url": "/flip/static/media/10-eficiencia-solar1.f2925c78.jpg"
+  }
+]);
